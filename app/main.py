c352d2d78de160d304a792e110c40de874d1e737
Andrey Potapov <90584170+AndreyPotapovv@users.noreply.github.com>
2025-06-13 00:13:01 +0900

    ver 0.9.11
    fix 7

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from routers import locations, admin
 from database import create_db_and_tables
 from fastapi.middleware.cors import CORSMiddleware
@@ -19,3 +20,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
