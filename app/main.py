0683904c26e53f895dd03fb52be06530795d4caf
Andrey Potapov <90584170+AndreyPotapovv@users.noreply.github.com>
2025-06-13 00:04:18 +0900

    ver 0.9.9
    fix 5

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.routers import locations, admin
-from app.database import create_db_and_tables
+from routers import locations, admin
+from database import create_db_and_tables
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Карта КФЕН")
